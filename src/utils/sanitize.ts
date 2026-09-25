b3591d3c72196bce52172cf9f2b7cfed1fa95514
Nachiket Narkhede <nachiketnarkhede18@gmail.com>
2026-09-25 11:55:21 +0530

    Delete src directory

--- src/utils/sanitize.ts
+++
@@ -1,5 +0,0 @@
-import DOMPurify from 'dompurify';
-
-export const cleanText = (raw: string): string => {
-  return DOMPurify.sanitize(raw, { ALLOWED_TAGS: ['b', 'i', 'em', 'strong', 'br', 'p'] });
-};
